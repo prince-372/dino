ed2dd053631d56567b2f3eda9d9579f616d3e662
prince-372 <pmor18314@gmail.com>
2024-02-09 10:27:05 +0530

    page modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,25 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        dino: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-100vw)" },
+        },
+        dino2: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(350px)" },
+        },
+      },
+      animation: {
+        cycle: "dino 20s linear infinite",
+        cycle2: "dino2 20s linear infinite",
+      },
+    },
     backgroundImage: {
       bgheader: "url(/src/assets/image/bgheaderr.webp)",
       BgBtnJoin: "url(/src/assets/image/bg-btn.png)",
     },
-    keyframes: {
-      dino: {
-        "0%": { transform: "translateX(0)" },
-        "100%": { transform: "translateX(-100vw)" },
-      },
-      dino2: {
-        "0%": { transform: "translateX(0)" },
-        "100%": { transform: "translateX(350px)" },
-      },
-    },
-    animation: {
-      cycle: "dino 20s linear infinite",
-      cycle2: "dino2 20s linear infinite",
-    },
+    
   },
   plugins: [],
 };
